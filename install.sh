df40f3d0a6f752024b545b275e46f18e1ca76a27
kvpavan9 <43925303+kvpavan9@users.noreply.github.com>
2020-02-25 23:14:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 sudo yum install tar wget vim unzip zip -y
-wget --user=admin --password=admin123 http://52.90.60.57:8081/repository/nexus/sample/sample.jar
+wget --user=admin --password=admin123 http://34.207.190.96:8081/repository/nexus/sample/sample.jar
